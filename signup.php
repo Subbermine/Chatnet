52f7664ffef7a7f6d3c960a4d5d463ae0740d3be
Saad Ansari <ansarisaad175@gmail.com>
2023-07-12 12:10:21 +0530

    First commit

---
+++ signup.php
@@ -0,0 +1,91 @@
+<?php require 'nav.php'; ?>
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    nav>a {
+        padding-left: 1rem;
+    }
+
+    h2 {
+        text-align: center;
+
+    }
+
+    .form-cont {
+        width: 40rem;
+        margin: auto;
+        margin-top: 3rem;
+        border: 1px solid black;
+        padding: 1rem;
+        height: 27rem;
+        overflow: hidden;
+    }
+
+    .form-check {
+        margin-top: 1rem;
+    }
+
+    .form-group {
+        margin-top: 2rem;
+    }
+
+    .form-group:nth-child(1) {
+        margin-top: 0;
+    }
+
+    #submit {
+        margin-top: 1rem;
+    }
+
+    #login {
+        text-align: right;
+    }
+
+    #terms {
+        color: rgb(13, 110, 253);
+        text-decoration: underline;
+    }
+
+    #terms:hover {
+        cursor: pointer;
+        color: blue;
+    }
+</style>
+
+<h2>Sign Up</h2>
+<div class='form-cont'>
+
+    <form action='signup.php' method='POST'>
+        <div class='form-group'>
+            <label for='exampleInputEmail1'>Email address</label>
+            <input name="email" type='email' class='form-control' id='exampleInputEmail1' aria-describedby='emailHelp'
+                placeholder='Enter email'>
+        </div>
+        <div class='form-group'>
+            <label for='exampleInputPassword1'>Password</label>
+            <input name="pass1" type='password' class='form-control' placeholder='Password'>
+        </div>
+        <div class='form-group'>
+            <label for='exampleInputPassword1'>Confirm Password</label>
+            <input name="pass2" type='password' class='form-control' placeholder='Password'>
+        </div>
+        <div class='form-check'>
+            <input type='checkbox' class='form-check-input' id='exampleCheck1' required>
+            <label class='form-check-label' for='exampleCheck1'>I have read the terms and conditions</label>
+        </div>
+        <div onclick="alert('There are no terms and conditions')" id='terms'>Terms and conditions</div>
+        <button type='submit' id='submit' class='btn btn-primary'>Submit</button>
+    </form>
+    <div id='login'>Already have an account? <a href=''>Login</a></div>
+</div>
+<?php
+if (isset($_POST['email']) && isset($_POST['pass1']) && isset($_POST['pass2'])) {
+    $email = $_POST['email'];
+    $pass1 = $_POST['pass1'];
+    $pass2 = $_POST['pass2'];
+}
+?>
