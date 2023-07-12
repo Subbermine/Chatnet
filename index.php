c7144c773a5b8a7a6b901dd55086d301eb3a3626
Saad Ansari <ansarisaad175@gmail.com>
2023-07-12 15:23:18 +0530

    index added

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php require "nav.php" ?>
+<style>
+    .form-cont {
+        width: 40rem;
+        margin: auto;
+        margin-top: 3rem;
+        border: 1px solid black;
+        padding: 1rem;
+        height: 15rem;
+        overflow: hidden;
+    }
+
+    .form-group:nth-child(2) {
+        margin-top: 1rem;
+    }
+
+    .form-check,
+    #submit {
+        margin-top: 1rem;
+    }
+
+    h2 {
+        text-align: center;
+        margin-top: 1rem;
+    }
+
+    #signup {
+        text-align: right;
+    }
+</style>
+
+<body>
+    <h2>Login</h2>
+    <div class="form-cont">
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input name="email" type="email" class="form-control" id="exampleInputEmail1"
+                    aria-describedby="emailHelp" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input name="password" type="password" class="form-control" id="exampleInputPassword1"
+                    placeholder="Password">
+            </div>
+            <button type="submit" id="submit" class="btn btn-primary">Login</button>
+        </form>
+        <div id="signup">New here? <a href="signup.php">Sign up</a></div>
+    </div>
+</body>
